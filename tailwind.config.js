3030d4368b2376895e460f143f7d75760e506df0
Pablo Ramos <pabloramosport725@gmail.com>
2021-04-24 19:37:56 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        pltGray: '#2f5d62',
+        pltDarkgray: '#364547',
+        pltYellow: '#ffe268',
+        pltOrange: '#ffb037'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
